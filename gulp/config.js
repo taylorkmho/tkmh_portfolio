f1424dcb69ee679ed093e81b08e31abd6831e340
Taylor Ho <taylorkmho@gmail.com>
2016-03-02 23:28:01 -0800

    Updated core - gulp/npm overhaul

---
+++ gulp/config.js
@@ -0,0 +1,53 @@
+/*
+  FILE PATHS
+  ==============
+*/
+
+var paths = {
+  base: {
+    root: '',
+    src: './src/',
+    dist: './dist/',
+    tmp: './tmp/'
+  }
+};
+
+paths.src = {
+  css: paths.base.src + 'assets/css',
+  // data: paths.base.src + 'data',
+  // fonts: paths.base.src + 'assets/fonts',
+  js: paths.base.src + 'assets/js',
+  images: paths.base.src + 'assets/images',
+  html: paths.base.src + 'html'
+};
+
+paths.dist = {
+  css: paths.base.dist + 'assets/css',
+  // fonts: paths.base.dist + 'assets/fonts',
+  js: paths.base.dist + 'assets/js',
+  images: paths.base.dist + 'assets/images',
+  html: paths.base.dist + ''
+};
+
+/*
+  ERROR HANDLING
+  ==============
+*/
+
+function swallowError (error) {
+  //If you want details of the error in the console
+  console.log(error.toString());
+
+  // Optional Beep
+  process.stdout.write('\x07');
+  this.emit('end');
+}
+
+/*
+  BROWSER REQUIREMENTS
+  ====================
+*/
+
+var browserReqs = ['last 2 versions', 'ie >= 9'];
+
+module.exports = { paths, swallowError, browserReqs };
---
+++ gulp/tasks/css.js
@@ -0,0 +1,31 @@
+var gulp              = require('gulp'),
+    paths             = require('../config').paths,
+    errorHandler      = require('../config').swallowError,
+    browserReqs       = require('../config').browserReqs,
+    sourcemaps        = require('gulp-sourcemaps'),
+    postcss           = require('gulp-postcss'),
+    autoprefixer      = require('autoprefixer'),
+    lost              = require('lost'),
+    postcssEasyImport = require('postcss-easy-import'),
+    postcssNested     = require('postcss-nested'),
+    postcssPXtoREM    = require('postcss-pxtorem'),
+    postcssSimpleVars = require('postcss-simple-vars');
+
+gulp.task('css', function() {
+
+  var postCSSProcessors = [
+        postcssEasyImport({ glob: true, path: ['./node_modules', './bower_components'] }),
+        postcssNested,
+        postcssPXtoREM,
+        postcssSimpleVars,
+        autoprefixer({ browsers: browserReqs }),
+        lost
+      ];
+
+  return gulp.src(paths.src.css + "/app.css")
+    .pipe(sourcemaps.init())
+    .pipe(postcss(postCSSProcessors).on('error', errorHandler))
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest(paths.dist.css))
+    .on('error', errorHandler)
+});
