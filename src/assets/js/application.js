20e6ad4aec35c0d9830367ad6583ff7afc79f520
Taylor Ho <taylorkmho@gmail.com>
2016-04-08 00:12:43 -0700

    Added motion overlay for projects

--- src/assets/js/application.js
+++ src/assets/js/application.js
@@ -2,6 +2,25 @@ require('es6-shim');
 
 let Blazy              = require('blazy');
 
-let blazy = new Blazy({
-  selector: '.lazyloaded'
-});
+export default class LazyLoader {
+  constructor(selector, hoverSelector) {
+    this.selector        = selector;
+    this.hoverSelector   = hoverSelector;
+    this.initBlazy();
+  }
+  initBlazy() {
+    this.blazy = new Blazy({
+      selector: this.selector,
+      successClass: 'lazyloaded',
+      errorClass: 'lazyerrored',
+      success: (el) => {
+
+      },
+      error: (el) => {
+
+      }
+    });
+  }
+}
+
+let lazyloader = new LazyLoader('.lazyload')
