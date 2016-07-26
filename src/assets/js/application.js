01ae09a70a536c38f3faf915061515cacd74cf21
Taylor Ho <taylorkmho@gmail.com>
2016-07-26 13:27:01 -0700

    Spec'ing out things todo inline
    sliding in one fix per barry’s suggestion

--- src/assets/js/application.js
+++ src/assets/js/application.js
@@ -77,7 +77,10 @@ let blazy = new Blazy({
 /*
   Scrollmagic - Splash
 */
-
+/*
+  TODO: Fix jumpiness. (ref: paul)
+        Utilize web worker? Might be difficult without being able to reference the DOM.
+*/
 let controller  = new ScrollMagic.Controller();
 
 if (document.body.getAttribute('data-template') === 'home') {
