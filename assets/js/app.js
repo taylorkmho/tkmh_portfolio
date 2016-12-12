4d5b4fe49d9122a5a778384d3dc33c4f70045caf
Taylor Ho <taylorkmho@gmail.com>
2016-12-12 01:39:46 -0800

    Update 2016-12-12T09:39:44.209Z

--- assets/js/app.js
+++ assets/js/app.js
@@ -278,20 +278,38 @@ c.family+'",serif'));z(p,function(a){h=a;r()});w(p,I(c,'"'+c.family+'",monospace
 },{}],3:[function(require,module,exports){
 'use strict';
 
-var _helpers = require("./lib/_helpers");
+var _helpers = require('./lib/_helpers');
 
-var _carousel = require("./lib/_carousel");
+var _carousel = require('./lib/_carousel');
 
 var _carousel2 = _interopRequireDefault(_carousel);
 
-var _videoBg = require("./lib/_video-bg");
+var _videoBg = require('./lib/_video-bg');
 
 var _videoBg2 = _interopRequireDefault(_videoBg);
+
+var _splashController = require('./lib/_splash-controller');
+
+var _splashController2 = _interopRequireDefault(_splashController);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Blazy = require('blazy');
 var FontFaceObserver = require('fontfaceobserver');
+
+/*
+  mobile test
+*/
+window.mobilecheck = function () {
+  var check = false;
+  (function (a) {
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+  })(navigator.userAgent || navigator.vendor || window.opera);
+  return check;
+};
+if (window.mobilecheck()) {
+  document.documentElement.classList.add('mobile-device');
+}
 
 /*
   .no-js management
@@ -363,91 +381,14 @@ var blazy = new Blazy({
 /*
   Scrollmagic - Splash
 */
-var controller = new ScrollMagic.Controller();
+window.onbeforeunload = function () {
+  $('body').css('opacity', 0);
+  window.scrollTo(0, 0);
+};
 
-if (document.body.getAttribute('data-template') === 'home') {
-  var splashEl = $('.splash');
-  var splashBGTween = TweenLite.to(splashEl, 1, {
-    backgroundColor: getComputedStyle($('.projects--main')[0]).getPropertyValue('background-color')
-  });
-  var splashBGScene = new ScrollMagic.Scene({
-    triggerElement: $('.projects--main')[0],
-    triggerHook: '.33',
-    duration: '33%'
-  }).setTween(splashBGTween).addTo(controller);
-
-  var splashTitleEl = $('.splash__title');
-  var splashTitleAnim = TweenMax.to(splashTitleEl, 1, {
-    y: '40%',
-    ease: Linear.easeNone
-  });
-  var splashTitleScene = new ScrollMagic.Scene({
-    triggerElement: splashEl,
-    triggerHook: 'onLeave',
-    duration: '100%'
-  }).setTween(splashTitleAnim).addTo(controller);
-
-  var splashSiloEl = $('.splash__silhouette');
-  var splashSiloAnim = TweenMax.to(splashSiloEl, 1, {
-    y: '60%',
-    ease: Linear.easeNone
-  });
-  var splashSiloScene = new ScrollMagic.Scene({
-    triggerElement: splashEl,
-    triggerHook: 'onLeave',
-    duration: '100%'
-  }).setTween(splashSiloAnim).addTo(controller);
-
-  var projectsMainEl = $('.projects--main');
-  var projectsSnippetsEl = $('.projects--snippets');
-  var projectsMainTween = TweenLite.to(projectsMainEl, 1, {
-    backgroundColor: getComputedStyle(projectsSnippetsEl[0]).getPropertyValue('background-color')
-  });
-  var projectsMainScene = new ScrollMagic.Scene({
-    triggerElement: projectsSnippetsEl[0],
-    triggerHook: '.33',
-    duration: '33%'
-  }).setTween(projectsMainTween).addTo(controller);
-
-  var projectsSnippetsTween = TweenLite.fromTo(projectsSnippetsEl, 1, {
-    backgroundColor: getComputedStyle(projectsSnippetsEl[0]).getPropertyValue('color'),
-    color: getComputedStyle(projectsSnippetsEl[0]).getPropertyValue('background-color')
-  }, {
-    color: getComputedStyle(projectsSnippetsEl[0]).getPropertyValue('color'),
-    backgroundColor: getComputedStyle(projectsSnippetsEl[0]).getPropertyValue('background-color')
-  });
-  var projectsSnippetsScene = new ScrollMagic.Scene({
-    triggerElement: projectsSnippetsEl[0],
-    triggerHook: '.33',
-    duration: '33%'
-  }).setTween(projectsSnippetsTween).addTo(controller);
+if (document.querySelector('.splash') && !window.mobilecheck()) {
+  var splashController = new _splashController2.default();
 }
-
-/*
-  Scrollmagic - Global
-*/
-
-var contactBGEl = $('.contact__bg');
-var contactBGAnim = TweenMax.from(contactBGEl, 1, {
-  y: -20 + '%'
-});
-var contactBGScene = new ScrollMagic.Scene({
-  triggerElement: $('.footer')[0],
-  triggerHook: 'onEnter',
-  duration: '100%'
-}).setTween(contactBGAnim).addTo(controller);
-
-var contactContainerEl = $('.contact__container');
-var contactContainerAnim = TweenMax.from(contactContainerEl, 1, {
-  y: 100 + '%',
-  opacity: 0
-});
-var contactContainerScene = new ScrollMagic.Scene({
-  triggerElement: $('.footer')[0],
-  triggerHook: 'onEnter',
-  duration: '80%'
-}).setTween(contactContainerAnim).addTo(controller);
-
 /*
   Projects - Image Carousel
 */
@@ -482,10 +423,17 @@ $('a[data-modal="carousel"]').magnificPopup({
         return images;
       };
       // parse elements into modal content
+      function getLink() {
+        if (data.url) {
+          return '<a class="btn btn--filled btn--filled-primary" href="' + data.url + '" target="_blank">View site</a>';
+        } else {
+          return '';
+        }
+      }
       item.src = '<div class="showcase">' + '<div class="showcase__container">' + '<div class="showcase__images">' + '<div class="Wallop"><div class="Wallop-list">' + buildCarouselImages(data.imagesList) + '</div></div>' + // Wallop, Wallop-list
       '<div class="Wallop-pagination">' + '<button class="Wallop-pagination__arrow Wallop-pagination__arrow--prev"><img src="assets/images/svg/icon-arrow.svg" /></a></button>' + buildCarouselPag(data.imagesList) + '<button class="Wallop-pagination__arrow Wallop-pagination__arrow--next"><img src="assets/images/svg/icon-arrow.svg" /></a></button>' + '</div>' + //Wallop-pagination
       '</div>' + // showcase__images
-      '<div class="showcase__description">' + '<img src="/assets/images/svg/logo-' + data.abbr + '.svg" alt="' + data.name + ' logo" title="' + data.name + ' logo" />' + '<p>' + data.description + '</p>' + '<a class="btn btn--filled btn--filled-primary" href="' + data.url + '" target="_blank">View site</a>' + '</div>' + // showcase__description
+      '<div class="showcase__description">' + '<img src="/assets/images/svg/logo-' + data.abbr + '.svg" alt="' + data.name + ' logo" title="' + data.name + ' logo" />' + '<p>' + data.description + '</p>' + getLink() + '</div>' + // showcase__description
       '</div>' + // showcase__container
       '</div>'; // showcase
     },
@@ -517,7 +465,9 @@ $('a[data-modal="carousel"]').magnificPopup({
   About video bg
 */
 
-var videoBG = new _videoBg2.default('.video-bg');
+if (document.querySelector('.video-bg')) {
+  var videoBG = new _videoBg2.default('.video-bg');
+}
 
 /*
   Set height for mobile - Splash
@@ -538,7 +488,7 @@ function widthUpdateHandler(mq) {
   }
 }
 
-},{"./lib/_carousel":4,"./lib/_helpers":5,"./lib/_video-bg":6,"blazy":1,"fontfaceobserver":2}],4:[function(require,module,exports){
+},{"./lib/_carousel":4,"./lib/_helpers":5,"./lib/_splash-controller":6,"./lib/_video-bg":7,"blazy":1,"fontfaceobserver":2}],4:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -654,6 +604,114 @@ function timer(time) {
 }
 
 },{}],6:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var SplashController = function () {
+  function SplashController(animSettings) {
+    var _this = this;
+
+    _classCallCheck(this, SplashController);
+
+    this.elements = {
+      splash: {
+        silhouette: document.querySelector('.splash__silhouette'),
+        title: document.querySelector('.splash__title'),
+        bg: document.querySelector('.splash__bg'),
+        arrow: document.querySelector('.splash__arrow')
+      },
+      projects: {
+        container: document.querySelector('.projects--main'),
+        title: document.querySelector('.projects--main .section-title'),
+        list: document.querySelector('.projects--main .projects__list')
+      }
+    };
+    this.animSettings = animSettings ? animSettings : {
+      duration: 500,
+      easing: 'ease-out',
+      delay: 0,
+      iterations: 1,
+      direction: 'normal',
+      fill: 'forwards'
+    };
+
+    this.elements.projects.title.style.opacity = 0;
+    this.elements.projects.list.style.opacity = 0;
+
+    this.elements.splash.arrow.addEventListener('click', this.scrollHandler);
+
+    var waypoint = new Waypoint({
+      element: this.elements.projects.title,
+      handler: function handler(direction) {
+        if (direction === 'down') {
+          _this.hideSplashContent();
+          _this.showProjectsContent();
+        } else {
+          _this.showSplashContent();
+          _this.hideProjectsContent();
+        }
+      },
+      offset: '90%'
+    });
+  }
+
+  _createClass(SplashController, [{
+    key: 'scrollHandler',
+    value: function scrollHandler() {
+      setTimeout(function () {
+        history.pushState("", document.title, window.location.pathname + window.location.search);
+      }, 100);
+    }
+  }, {
+    key: 'hideSplashContent',
+    value: function hideSplashContent() {
+      this.animSettings.delay = 0;
+      this.elements.splash.silhouette.animate([{ transform: 'scale(1) translateX(-50%)', opacity: 1, offset: 0 }, { transform: 'scale(.9) translateX(-50%)', opacity: 0, offset: 1 }], this.animSettings);
+      this.elements.splash.title.animate([{ opacity: 1, offset: 0 }, { opacity: 0, offset: 1 }], this.animSettings);
+      this.elements.splash.bg.animate([{ opacity: 1, offset: 0 }, { opacity: 0, offset: 1 }], this.animSettings);
+      this.elements.splash.arrow.animate([{ opacity: 1, offset: 0 }, { opacity: 0, offset: 1 }], this.animSettings);
+      this.elements.splash.arrow.style.pointerEvents = 'none';
+    }
+  }, {
+    key: 'showSplashContent',
+    value: function showSplashContent() {
+      this.animSettings.delay = 0;
+      this.elements.splash.silhouette.animate([{ transform: 'scale(.9) translateX(-50%)', opacity: 0, offset: 0 }, { transform: 'scale(1) translateX(-50%)', opacity: 1, offset: 1 }], this.animSettings);
+      this.elements.splash.title.animate([{ opacity: 0, offset: 0 }, { opacity: 1, offset: 1 }], this.animSettings);
+      this.elements.splash.bg.animate([{ opacity: 0, offset: 0 }, { opacity: 1, offset: 1 }], this.animSettings);
+      this.elements.splash.arrow.animate([{ opacity: 0, offset: 0 }, { opacity: 1, offset: 1 }], this.animSettings);
+      this.elements.splash.arrow.style.pointerEvents = 'initial';
+    }
+  }, {
+    key: 'showProjectsContent',
+    value: function showProjectsContent() {
+      this.animSettings.delay = 250;
+      this.elements.projects.title.animate([{ transform: 'translateY(40px)', opacity: 0, offset: 0 }, { transform: 'translateY(0px)', opacity: 1, offset: 1 }], this.animSettings);
+      this.animSettings.delay = 500;
+      this.elements.projects.list.animate([{ transform: 'translateY(40px)', opacity: 0, offset: 0 }, { transform: 'translateY(0px)', opacity: 1, offset: 1 }], this.animSettings);
+    }
+  }, {
+    key: 'hideProjectsContent',
+    value: function hideProjectsContent() {
+      this.animSettings.delay = 0;
+      this.elements.projects.title.animate([{ transform: 'translateY(0px)', opacity: 1, offset: 0 }, { transform: 'translateY(40px)', opacity: 0, offset: 1 }], this.animSettings);
+      this.elements.projects.list.animate([{ transform: 'translateY(40px)', opacity: 1, offset: 0 }, { transform: 'translateY(0px)', opacity: 0, offset: 1 }], this.animSettings);
+    }
+  }]);
+
+  return SplashController;
+}();
+
+exports.default = SplashController;
+
+},{}],7:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
